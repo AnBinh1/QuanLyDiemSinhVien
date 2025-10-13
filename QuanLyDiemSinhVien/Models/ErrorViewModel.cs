34c210610cd27c37083e11a176bc067f59b91b6c
AnBinh1 <anhohoang2004@gmail.com>
2025-10-13 16:57:20 +0700

    Add project files.

---
+++ QuanLyDiemSinhVien/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyDiemSinhVien.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
